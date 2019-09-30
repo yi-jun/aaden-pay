1f94f99f8bc745178fb4d4dde808f09eec60bbc1
ning.wang <ning.wang@zhan.com>
2019-09-30 18:56:09 +0800

    get bean by spring content

--- pay-service/src/main/java/com/aaden/pay/service/biz/route/ThirdBankRoute.java
+++ pay-service/src/main/java/com/aaden/pay/service/biz/route/ThirdBankRoute.java
@@ -1,14 +1,9 @@
 package com.aaden.pay.service.biz.route;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.aaden.pay.api.biz.vo.BankRequest;
-import com.aaden.pay.core.utils.ClassUtil;
-import com.aaden.pay.core.utils.SpringContextHelper;
 import com.aaden.pay.service.biz.annotation.ChannelValue;
 import com.aaden.pay.service.biz.tp.ThirdBankVerifyService;
+import org.springframework.stereotype.Service;
 
 /**
  *  @Description 第三方绑卡签约适配
@@ -16,33 +11,35 @@ import com.aaden.pay.service.biz.tp.ThirdBankVerifyService;
  *  @date 2017年12月26日
  */
 @Service
-public class ThirdBankRoute {
-
-	private static List<Class<?>> thirdClassList = ClassUtil.getAllSubClass(ThirdBankVerifyService.class);
+public class ThirdBankRoute extends AbstractRoute<ThirdBankVerifyService> {
 
 	public ThirdBankVerifyService route(BankRequest request) throws Exception {
 		if (request == null || request.getInfo().getChannel() == null) {
 			throw new Exception("当前暂不支持的第三方支付渠道");
 		}
 
-		if (thirdClassList == null)
+		if (thirdBanks == null)
 			throw new Exception("当前暂不支持的第三方支付渠道");
 
-		for (Class<?> clz : thirdClassList) {
-			ChannelValue comment = clz.getAnnotation(ChannelValue.class);
+		for (ThirdBankVerifyService bean : thirdBanks) {
+			ChannelValue comment = bean.getClass().getAnnotation(ChannelValue.class);
 			if (comment == null)
 				continue;
 			if (comment.channel() != request.getInfo().getChannel())
 				continue;
 
-			Service service = clz.getAnnotation(Service.class);
+			Service service = bean.getClass().getAnnotation(Service.class);
 			if (service == null)
 				continue;
 
-			return (ThirdBankVerifyService) SpringContextHelper.getBean(service.value());
+			return bean;
 		}
 
 		throw new Exception("当前暂不支持的第三方支付渠道");
 	}
 
+	@Override
+	Class<ThirdBankVerifyService> getClazz() {
+		return ThirdBankVerifyService.class;
+	}
 }
